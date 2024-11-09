6e06104a7dcaebadbaaa0e7f022977fcf2d936f0
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-11-10 00:29:35 +0200

    server js update

--- winx-club-quiz/backend/server.js
+++ winx-club-quiz/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const quizRoutes = require('./routes/quizRoutes'); 
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
+app.use('/api', quizRoutes);
 
 const port = process.env.PORT || 5000;
 
